78e78caa23fdc5e39a69ecbc3dfe65fe0ad2f299
Dragonmaster129 <dragons4756@gmail.com>
2021-10-19 08:09:31 -0600

    added post topics to post component

--- src/components/post.js
+++ src/components/post.js
@@ -5,6 +5,22 @@ export default class Post extends Component {
     super(props);
   }
   render() {
-    return <li>{this.props.title}</li>;
+    const associated_topics = this.props.associated_topics.map(
+      (topic, index) => {
+        return (
+          <span className="post-topic" key={index}>
+            {topic}
+          </span>
+        );
+      }
+    );
+    return (
+      <li className="recent-post">
+        <div className="recent-post-title">
+          <h3>{this.props.title}</h3>
+        </div>
+        <div className="recent-post-topics">{associated_topics}</div>
+      </li>
+    );
   }
 }
